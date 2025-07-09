5def62b55d76a03a0f0f8209a5d1feebdcae4ab5
neo <neo.konikoff-garaix01@etu.umontpellier.fr>
2025-07-09 10:07:13 +0200

    API JS beginning

---
+++ wps/www/js/ogc_elements/Job.js
@@ -0,0 +1,79 @@
+import {ApiJob} from "../api/ApiJob";
+
+export class Job {
+
+    constructor(options) {
+        this._created = options.created;
+        this._started = options.started ? options.started : '';
+        this._expire = options.expire ? options.expire : '';
+        this._finished = options.finished ? options.finished : '';
+        this._updated = options.updated;
+
+        this._jobID = options.jobID;
+
+        this._links = options.links;
+
+        this._message = options.message;
+
+        this._processID = options.processID;
+        this._cleanProcessID = options.processID.replaceAll(':', '-');
+
+        this._progress = options.progress;
+
+        this._status = options.status;
+
+        this._type = options.type;
+    }
+
+    getTimes() {
+        return {
+            "created" : this._created,
+            "started" : this._started,
+            "expire" : this._expire,
+            "finished" : this._finished,
+            "updated" : this._updated
+        }
+    }
+
+    async getResults() {
+        const areResultsUp = this._links[2].href;
+
+        if (!areResultsUp)
+            return undefined;
+
+        return await ApiJob.getResultOfSpecificJob(this._jobID);
+
+    }
+
+    getJobID() {
+        return this._jobID;
+    }
+
+    getLinks() {
+        return this._links;
+    }
+
+    getMessage() {
+        return this._message;
+    }
+
+    getProcessID() {
+        return this._processID;
+    }
+
+    getCleanProcessID() {
+        return this._cleanProcessID;
+    }
+
+    getProgress() {
+        return this._progress;
+    }
+
+    getStatus() {
+        return this._status;
+    }
+
+    getType() {
+        return this._type;
+    }
+}
